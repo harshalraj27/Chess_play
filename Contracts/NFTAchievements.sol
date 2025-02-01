7b635c3ef490ddee045726e30bc4d275011e4221
harshalraj27 <harshalraj27@gmail.com>
2025-02-01 17:05:52 +0530

    Update NFTAchievements.sol

--- Contracts/NFTAchievements.sol
+++ Contracts/NFTAchievements.sol
@@ -2,53 +2,57 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTAchievements is ERC721 {
+contract NFTAchievements is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
     mapping(address => uint256) public playerRatings;  
-    mapping(address => uint256) public playerNFTs;  
+    mapping(address => uint256[]) public playerNFTs;  
 
-    uint256 public milestone1 = 1000;
-    uint256 public milestone2 = 1500;
-    uint256 public milestone3 = 2000;
-    uint256 public milestone4 = 2500;
-    uint256 public milestone5 = 3000;
+    uint256[] public milestones = [1000, 1500, 2000, 2500, 3000];
 
-    constructor() ERC721("ChessAchievementNFT", "CANFT") {}
+    constructor() ERC721("ChessAchievementNFT", "CANFT") Ownable(msg.sender) {}
 
-    function setPlayerRating(address player, uint256 rating) external {
+    function setPlayerRating(address player, uint256 rating) external onlyOwner {
         playerRatings[player] = rating;
         checkAndMintNFT(player);
     }
 
     function checkAndMintNFT(address player) internal {
         uint256 playerRating = playerRatings[player];
-        
-        if (playerRating >= milestone5 && playerNFTs[player] == 0) {
-            mintNFT(player);
-        } else if (playerRating >= milestone4 && playerNFTs[player] == 0) {
-            mintNFT(player);
-        } else if (playerRating >= milestone3 && playerNFTs[player] == 0) {
-            mintNFT(player);
-        } else if (playerRating >= milestone2 && playerNFTs[player] == 0) {
-            mintNFT(player);
-        } else if (playerRating >= milestone1 && playerNFTs[player] == 0) {
-            mintNFT(player);
+
+        for (uint256 i = 0; i < milestones.length; i++) {
+            if (playerRating >= milestones[i] && !hasAchievedMilestone(player, milestones[i])) {
+                mintNFT(player);
+            }
         }
     }
 
+    function hasAchievedMilestone(address player, uint256 milestone) internal view returns (bool) {
+        for (uint256 i = 0; i < playerNFTs[player].length; i++) {
+            if (playerNFTs[player][i] == milestone) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function mintNFT(address player) internal {
+        _tokenIdCounter.increment();
         uint256 tokenId = _tokenIdCounter.current();
         _safeMint(player, tokenId);
-        _tokenIdCounter.increment();
 
-        playerNFTs[player] = tokenId;  
+        playerNFTs[player].push(tokenId);  
     }
 
     function getPlayerRating(address player) external view returns (uint256) {
         return playerRatings[player];
+    }
+
+    function getPlayerNFTs(address player) external view returns (uint256[] memory) {
+        return playerNFTs[player];
     }
 }
